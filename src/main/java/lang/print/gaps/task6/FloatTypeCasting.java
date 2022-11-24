21c4f8166659ed92e8abf837c275de8d12fca17e
Sergey Khaykov <ssserg3000@gmail.com>
2022-11-24 22:13:59 +0200

    initial commit

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,19 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+
+        int intNumber = (int) numberToBeRounded;
+//        System.out.println(numberToBeRounded);
+//        System.out.println(intNumber);
+//        System.out.println(numberToBeRounded-intNumber);
+        if (numberToBeRounded - intNumber < 0.5) {
+            System.out.println(intNumber);
+        } else System.out.println(intNumber + 1);
+
+    }
+
+    public static void main(String[] args) {
+        FloatTypeCasting ftc = new FloatTypeCasting();
+        ftc.roundNumber(9.5f);
     }
 }
